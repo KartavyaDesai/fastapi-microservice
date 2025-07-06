c03bccc9ec824866b00d65b23dcb4fb7211eed29
KartavyaDesai <126103657+KartavyaDesai@users.noreply.github.com>
2025-07-06 17:29:45 +0000

    Enhanced logic, made api, updated requirements and cli file

--- cli-fire.py
+++ cli-fire.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # The shebang line tells to run it as a script. Register it via chmod +x cli-fire.py
 import fire
-from projectlib.logic import wiki
+from projectlib import logic
 
 if __name__ == "__main__":
-    fire.Fire(wiki)
+    fire.Fire(logic)
